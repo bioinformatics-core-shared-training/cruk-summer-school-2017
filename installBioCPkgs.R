abeee8b3f797b13dbd3e9c580de132f4b60065b9
Mark Dunning <mark.dunning@gmail.com>
2017-07-20 11:38:32 +0100

    Add new packages to R install script

--- installBioCPkgs.R
+++ installBioCPkgs.R
@@ -5,7 +5,8 @@ biocLite(c("Biostrings", "biomaRt", "BSgenome","QDNAseq","exomeCopy",
         "rtracklayer", "ggbio", "Gviz","RColorBrewer","org.Hs.eg.db",
         "TxDb.Hsapiens.UCSC.hg19.knownGene","BSgenome.Hsapiens.UCSC.hg19",
         "wakefield","VariantAnnotation","limma","dplyr","ggplot2","tidyr","readr",
-        "GenVisR","circlize","RColorBrewer","circlize", "InteractionSet","mclust","devtools","SomaticSignatures","deconstructSigs","COSMIC.76"))
+        "GenVisR","circlize","RColorBrewer","circlize", "InteractionSet","mclust","devtools","SomaticSignatures","deconstructSigs","COSMIC.76",
+        "ComplexHeatmaps","plyr","reshape"))
         
 library(devtools)
 install_github("Crick-CancerGenomics/ascat/ASCAT")
